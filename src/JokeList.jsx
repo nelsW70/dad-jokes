4a074c80b205ba493e6d277f91d107e1b89e27e3
Nelson Foltz <nelsfoltz@gmail.com>
2020-09-15 11:59:44 -0400

    final cleanup

--- src/JokeList.jsx
+++ src/JokeList.jsx
@@ -88,7 +88,7 @@ export class JokeList extends Component {
             alt="laughing emoji"
           />
           <button className="JokeList-getmore" onClick={this.handleClick}>
-            New Jokes
+            Fetch Jokes
           </button>
         </div>
         <div className="JokeList-jokes">
