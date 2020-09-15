53f411c28ac4024d0ff015e20ec227b3c7d254fb
Nelson Foltz <nelsfoltz@gmail.com>
2020-09-15 10:49:58 -0400

    add: spinner for loading new jokes

--- src/JokeList.jsx
+++ src/JokeList.jsx
@@ -10,7 +10,8 @@ export class JokeList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      jokes: JSON.parse(window.localStorage.getItem('jokes') || '[')
+      jokes: JSON.parse(window.localStorage.getItem('jokes') || '['),
+      loading: false
     };
     this.handleClick = this.handleClick.bind(this);
   }
@@ -29,6 +30,7 @@ export class JokeList extends Component {
     }
     this.setState(
       st => ({
+        loading: false,
         jokes: [...st.jokes, ...jokes]
       }),
       () =>
@@ -49,10 +51,18 @@ export class JokeList extends Component {
   }
 
   handleClick() {
-    this.getJokes();
+    this.setState({ loading: true }, this.getJokes);
   }
 
   render() {
+    if (this.state.loading) {
+      return (
+        <div className="JokeList-spinner">
+          <i className="far fa-8x fa-laugh fa-spin"></i>
+          <h1 className="JokeList-title">Loading...</h1>
+        </div>
+      );
+    }
     return (
       <div className="JokeList">
         <div className="JokeList-sidebar">
